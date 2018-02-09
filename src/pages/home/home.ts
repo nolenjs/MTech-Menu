51332e268a1aa77392e0b2b4523546b2f9b0ca5a
Alex <alexdahl1999@gmail.com>
2018-02-09 09:38:56 -0700

    added some buttons for navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {MenuPage} from '../menu/menu';
+import {LoginPage} from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,13 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotToHome(){
+    this.navCtrl.push(MenuPage);
+  }
+  gotToLogin(){
+    this.navCtrl.push(LoginPage);
   }
 
 }
