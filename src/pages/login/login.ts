c28d6b5bbe3fd6a04eaf8855ccda7ec7c029bf45
nolenjs <nolenshubin@gmail.com>
2018-02-12 09:22:34 -0700

    Linked up with latest project, pushing login page updates

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,6 +10,8 @@ export class LoginPage {
 
   email: string;
   password: string;
+  password2: string;
+  htmlToAdd: string;
 
   constructor(public navCtrl: NavController,
               private firebase: AngularFireAuth) {
@@ -17,20 +19,23 @@ export class LoginPage {
   }
 
   signUp(){
-    this.firebase.auth.createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
-      // Handle Errors here.
-      console.log(this.email)
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      // [START_EXCLUDE]
-      if (errorCode == 'auth/weak-password') {
-        alert('The password is too weak.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-      // [END_EXCLUDE]
-    });
+    if (this.password === this.password2){
+      console.log("Passords match, signing up");
+      this.firebase.auth.createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
+        // Handle Errors here.
+        console.log(this.email);
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+    }
   }
 
   signIn(){
@@ -47,6 +52,13 @@ export class LoginPage {
       console.log(error);
       // [END_EXCLUDE]
     });
+  }
+
+  addPasswordInput(){
+    this.htmlToAdd = '<ion-item>' +
+      '        <ion-label floating>Confirm Password</ion-label>' +
+      '        <ion-input (keyUp)="password2" type="text"></ion-input>' +
+      '      </ion-item>';
   }
 
 }
